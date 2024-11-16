b76607cf8893a49c2a9198f586e61383a5f3f3a5
Russell Richardson <admin@russ.network>
2024-11-16 02:16:37 -0500

    Initial Progressive Web App support

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import './style.css'
 import App from './App.vue'
 import { router } from './router'
 import socketPlugin from "@/plugins/socket-plugin";
+import {registerSW} from "virtual:pwa-register";
 
 const pinia = createPinia();
 addIcons(HiInformationCircle, BiArrowDownUp, FaAngleDoubleUp, FaDownload, MdReplaycirclefilled, MdDeleteforever,
@@ -28,3 +29,15 @@ createApp(App)
     .use(pinia)
     .use(socketPlugin)
     .mount('#app')
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm("New content is available!. Refresh to update?")) {
+            updateSW().then(_ => {});
+            // TODO: Add a pleasant toast notification for this
+        }
+    },
+    onOfflineReady() {
+        alert("No internet connection found. Most of the app features may not work.");
+    }
+})
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,54 @@
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          devOptions: { enabled: true },
+          includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon-180x180.png'],
+          manifest: {
+              name: 'BitJourney Compass',
+              short_name: 'Compass',
+              description: 'Compass is an application for interacting with the BitJourney service',
+              theme_color: '#1c1917',
+              background_color: '#1c1917',
+              icons: [
+                  {
+                      src: 'pwa-64x64.png',
+                      sizes: '64x64',
+                      type: 'image/png',
+                  },
+                  {
+                      src: 'pwa-128x128.png',
+                      sizes: '128x128',
+                      type: 'image/png',
+                  },
+                  {
+                      src: 'pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                  },
+                  {
+                      src: 'maskable-icon-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'maskable'
+                  },
+                  {
+                      src: 'maskable-icon-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'any'
+                  }
+              ]
+          }
+      })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
